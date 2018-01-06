220e30c9672b405751eb92965518e1602be5e5ec
koteitan <ukaibutton@mac.com>
2018-01-06 21:47:48 +0900

    API test code for bitbank.cc is implemented.

--- test.py
+++ test.py
@@ -1,15 +1,23 @@
-from binance.client import Client
+import binance.client
+import python_bitbankcc
 from sys import exit
 import mykey
 import time
+import json # for binance
 
-client=Client(mykey.KEY, mykey.SECRET);
-result=client.get_klines(
+# test bitbank -------
+bbcl=python_bitbankcc.public();
+value=bbcl.get_candlestick('btc_jpy','1min','20180101');
+print(value);
+exit();
+
+# test binance -------
+bncl=binance.client.Client(mykey.BINANCE_KEY, mykey.BINANCE_SECRET);
+result=bncl.get_klines(
   symbol   ="ETHBTC",
-  interval =Client.KLINE_INTERVAL_1MINUTE,
-#  limit    =1,
-#  startTime=int(time.time()*1000),
-#  endTime  =int(time.time()*1000)
+  interval =binance.client.Client.KLINE_INTERVAL_1MINUTE,
+  limit    =1,
+  startTime=int(time.time()*1000-1000*60*10),
 )
 
 print(result)
