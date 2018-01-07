211095dd2465433885b5a90a72ad10fd8b015590
koteitan <ukaibutton@mac.com>
2018-01-07 21:40:36 +0900

    get ratelimit

--- folio.py
+++ folio.py
@@ -1,19 +1,36 @@
+#!/usr/bin/env python
 import binance.client
 import python_bitbankcc
 from sys import exit
 import mysetting
 import time
 import json # for binance
+import csv
 
-# test bitbank -------
-bbcl=python_bitbankcc.public();
-value=bbcl.get_candlestick('btc_jpy','1min','20180101');
-print(value);
+bnClient=binance.client.Client(mysetting.BINANCE_KEY, mysetting.BINANCE_SECRET);
+result=bnClient.get_exchange_info();
+#import pdb; pdb.set_trace()
+for e in result["rateLimits"]:
+  if e["rateLimitType"] == "REQUESTS":
+    accesslimit=e["limit"]
+print accesslimit
 exit();
 
+
+
+with open(mysetting.BINANCE_HIST,'r') as f:
+  reader = csv.reader(f)
+  header = next(reader)
+  for row in reader:
+    print row
+
+# test bitbank -------
+bbClient=python_bitbankcc.public();
+value=bbClient.get_candlestick('btc_jpy','1min','20180101');
+print(value);
+
 # test binance -------
-bncl=binance.client.Client(mysetting.BINANCE_KEY, mysetting.BINANCE_SECRET);
-result=bncl.get_klines(
+result=bnClient.get_klines(
   symbol   ="ETHBTC",
   interval =binance.client.Client.KLINE_INTERVAL_1MINUTE,
   limit    =1,
